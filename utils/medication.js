816dda834627c025d95b3cdbb45fe7c96e33f34e
nyugoh <nyugoh@gmail.com>
2017-10-24 18:23:05 +0300

    Fixed inventory module, issue was Medication and distribution unit not showing up after importing the inventory list. Make sure it is Medication not medication or medicine

---
+++ utils/medication.js
@@ -0,0 +1,52 @@
+var camelize = require('camelize');
+var config = require('../config.js');
+var fs = require('fs');
+var moment = require('moment');
+var nano = require('nano')(config.couchAuthDbURL);
+var parse = require('csv-parse');
+var relPouch = require('relational-pouch');
+var uuid = require('node-uuid');
+
+var maindb = nano.use('main');
+var views = ['inventory_by_friendly_id', 'inventory_by_name'];
+var updatedDocs = 0;
+var errDocs = 0;
+
+views.map(function(view){
+  processDocuments(view);
+});
+
+console.log('Util updated %s documents', updatedDocs);
+if (errDocs !== 0) {
+  console.log('%s documents not updated.', errDocs);
+}
+
+function processDocuments(viewName) {
+  maindb.view(viewName, viewName, { include_docs: true }, function(err, body){
+    if (!err) {
+      body.rows.map((doc)=>{
+        let item = doc.doc.data;
+        if (item.inventoryType == 'medicine' || item.inventoryType == 'medication') {
+          item.inventoryType = 'Medication';
+          updateDocument(doc.doc, viewName, function(res){
+            console.log(res);
+          });
+        }
+      });
+    } else {
+      console.log('Not connected to couchdb.');
+    }
+  });
+}
+
+function updateDocument(doc, viewName, callback){
+  maindb.insert(doc, doc._id, function(error, response){
+    if (error) {
+      errDocs += 1;
+      callback(error);
+    } else {
+      updatedDocs += 1;
+      callback(response);
+    }
+  });
+}
